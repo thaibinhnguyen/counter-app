f9c9e43a9eab94827e0459f087ae03deade1ceba
THAI Binh Nguyen <thaibinhnguyen26041996@gmail.com>
2020-05-29 15:14:39 +0200

    fix decrement button

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
     componentDidUpdate(prevProps, prevState) {}
-    state = {
-        value: this.props.counter.value,
-    };
-
     formatCount = () => {
         return this.props.counter.value === 0
             ? "Zero"
@@ -27,6 +23,7 @@ export default class Counter extends Component {
                 <button
                     onClick={() => this.props.onDecrement(this.props.counter)}
                     className="btn btn-secondary btn-sm m-2"
+                    disabled={!this.props.counter.value}
                 >
                     Decrement
                 </button>
